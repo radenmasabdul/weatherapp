c4940358b4542e3ae6c6a7522f2dae75cdd25a22
radenmasabdul <abdul.tamsis@gmail.com>
2023-02-11 15:10:43 +0700

    forecast today

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { useSelector } from "react-redux";
 
 const Widget = () => {
@@ -11,49 +10,43 @@ const Widget = () => {
   let k = cities?.main?.temp;
   let C = k - 273.15;
 
-  function formatAMPM(date) {
+  const formatAMPM = (date) => {
     var time = new Date(date);
     var strTime = time.toLocaleString("en-US", {
       hour: "numeric",
       hour12: true,
     });
     return strTime;
-  }
+  };
 
-  function convertToCelcius(suhu) {
+  const convertToCelcius = (suhu) => {
     let K = suhu;
     let Celcius = K - 273.15;
     return Celcius;
-  }
+  };
 
   return (
     <>
-      <div className="flex flex-col text-gray-700 my-2 mx-2">
-        <div className="w-full max-w-screen-sm bg-base-100 shadow-xl p-10 rounded-xl ring-8 ring-white ring-opacity-40">
+      <div className="flex flex-col my-2 mx-2">
+        <div className="w-full max-w-screen-sm bg-teal-300 shadow-xl p-10 rounded-xl ring-8 ring-white ring-opacity-40">
           <div className="flex justify-between">
             <div className="flex flex-col">
               <span className="text-6xl font-bold">{`${C.toFixed(2)}°C`}</span>
               <span className="font-semibold mt-1 text-gray-500">
                 {Object.keys(cities).length > 0
-                  ? `${cities.name} , ${cities?.sys?.country}`
+                  ? `${cities.name}, ${cities?.sys?.country}`
                   : ""}
               </span>
               <span>
                 {Object.keys(cities).length > 0
-                  ? `${cities?.weather?.[0]?.main} , ${cities?.weather?.[0]?.description}`
+                  ? `${cities?.weather?.[0]?.main}, ${cities?.weather?.[0]?.description}`
                   : ""}
               </span>
             </div>
-            <svg
-              className="h-24 w-24 fill-current text-gray-400"
-              xmlns="http://www.w3.org/2000/svg"
-              height={24}
-              viewBox="0 0 24 24"
-              width={24}
-            >
-              <path d="M19.78,17.51c-2.47,0-6.57-1.33-8.68-5.43C8.77,7.57,10.6,3.6,11.63,2.01C6.27,2.2,1.98,6.59,1.98,12 c0,0.14,0.02,0.28,0.02,0.42C2.61,12.16,3.28,12,3.98,12c0,0,0,0,0,0c0-3.09,1.73-5.77,4.3-7.1C7.78,7.09,7.74,9.94,9.32,13 c1.57,3.04,4.18,4.95,6.8,5.86c-1.23,0.74-2.65,1.15-4.13,1.15c-0.5,0-1-0.05-1.48-0.14c-0.37,0.7-0.94,1.27-1.64,1.64 c0.98,0.32,2.03,0.5,3.11,0.5c3.5,0,6.58-1.8,8.37-4.52C20.18,17.5,19.98,17.51,19.78,17.51z" />
-              <path d="M7,16l-0.18,0C6.4,14.84,5.3,14,4,14c-1.66,0-3,1.34-3,3s1.34,3,3,3c0.62,0,2.49,0,3,0c1.1,0,2-0.9,2-2 C9,16.9,8.1,16,7,16z" />
-            </svg>
+            <img
+              src={`http://openweathermap.org/img/wn/${cities?.weather?.[0]?.icon}.png`}
+              className="h-24 w-24 fill-current"
+            />
           </div>
           <div className="flex justify-between mt-12">
             {hourlyForecast.map(
@@ -61,20 +54,13 @@ const Widget = () => {
                 index < 8 && (
                   <div className="flex flex-col items-center" key={index}>
                     <span className="font-semibold text-lg">
-                      {convertToCelcius(data?.main?.temp).toFixed(2) + " °C"}
+                      {convertToCelcius(data?.main?.temp).toFixed(2) + "°C"}
                     </span>
-
-                    <svg
-                      className="h-10 w-10 fill-current text-gray-400 mt-3"
-                      xmlns="http://www.w3.org/2000/svg"
-                      height={24}
-                      viewBox="0 0 24 24"
-                      width={24}
-                    >
-                      <path d="M19.78,17.51c-2.47,0-6.57-1.33-8.68-5.43C8.77,7.57,10.6,3.6,11.63,2.01C6.27,2.2,1.98,6.59,1.98,12 c0,0.14,0.02,0.28,0.02,0.42C2.61,12.16,3.28,12,3.98,12c0,0,0,0,0,0c0-3.09,1.73-5.77,4.3-7.1C7.78,7.09,7.74,9.94,9.32,13 c1.57,3.04,4.18,4.95,6.8,5.86c-1.23,0.74-2.65,1.15-4.13,1.15c-0.5,0-1-0.05-1.48-0.14c-0.37,0.7-0.94,1.27-1.64,1.64 c0.98,0.32,2.03,0.5,3.11,0.5c3.5,0,6.58-1.8,8.37-4.52C20.18,17.5,19.98,17.51,19.78,17.51z" />
-                      <path d="M7,16l-0.18,0C6.4,14.84,5.3,14,4,14c-1.66,0-3,1.34-3,3s1.34,3,3,3c0.62,0,2.49,0,3,0c1.1,0,2-0.9,2-2 C9,16.9,8.1,16,7,16z" />{" "}
-                    </svg>
-
+                    <img
+                      src={`http://openweathermap.org/img/wn/${data?.weather?.[0]?.icon}.png`}
+                      alt="wthr img"
+                      className="h-10 w-10 fill-current my-2"
+                    />
                     <span className="font-semibold mt-1 text-sm text-center">
                       {formatAMPM(data?.dt_txt)}
                     </span>
@@ -374,7 +360,7 @@ const Widget7Days = () => {
 const WidgetOrderCity = () => {
   return (
     <div className="card flex items-center py-2">
-      <div className="flex flex-col justify-center items-center p-8 rounded-md w-60 sm:px-12 bg-base-100 shadow-xl dark:bg-gray-900 dark:text-gray-100">
+      <div className="flex flex-col justify-center items-center p-8 rounded-md w-60 sm:px-12 bg-teal-300 shadow-xl dark:bg-gray-900 dark:text-gray-100">
         <div className="text-center">
           <h2 className="text-xl font-semibold">Dubai</h2>
           <p className="text-sm dark:text-gray-400">July 29</p>
